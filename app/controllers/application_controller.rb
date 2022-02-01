a7aba7aeab7111f47db6f39e508470444ca69a54
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-01 17:02:43 +0000

    commit 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  def blank_add_form
+  render({ :template=> "calculation_templates/add_form.html.erb"})
+  end
+
+  def add_results
+    render({ :template=> "calculation_templates/add_results.html.erb"})
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
+  get("/add", ({ :controller=> "application", :action=>"blank_add_form"}))
+  get("/subtract", ({ :controller=> "application", :action=>"blank_subtract_form"}))
+  get("/multiply", ({ :controller=> "application", :action=>"blank_multiply_form"}))
+  get("/divide", ({ :controller=> "application", :action=>"blank_divide_form"}))
+
+  get("/add/results", ({ :controller=> "application", :action=>"add_results"}))
+  get("/subtract", ({ :controller=> "application", :action=>"subtract_results"}))
+  get("/multiply", ({ :controller=> "application", :action=>"multiply_results"}))
+  get("/divide", ({ :controller=> "application", :action=>"divide_results"}))
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
