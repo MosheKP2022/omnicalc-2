17c644a6dd1de283dcf2302b902ff04d33619ac1
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-08 02:36:38 +0000

    Street to Coordinates work basically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,4 +47,30 @@ class ApplicationController < ActionController::Base
           @result = @num1/@num2
           render ({ :template=> "calculation_templates/divide_results"})
         end
+
+
+        def street_to_coords_results
+          # Parameters: {"user_street_address"=>"Merchandise Mart, Chicago"}
+      
+          @street_address = params.fetch("user_street_address")
+      
+          @maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address + "&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+      
+          @raw_json_string = open(@maps_url).read
+      
+          @parsed_json = JSON.parse(@raw_json_string)
+      
+          @results_array = @parsed_json.fetch("results")
+      
+          @first_result = @results_array.at(0)
+          
+          @geometry_hash = @first_result.fetch("geometry")
+      
+          @location_hash = @geometry_hash.fetch("location")
+      
+          @latitude = @location_hash.fetch("lat")
+          @longitude = @location_hash.fetch("lng")
+      
+          render({ :template=> "weather_templates/street_to_coords_results.html.erb"})
+        end
 end
